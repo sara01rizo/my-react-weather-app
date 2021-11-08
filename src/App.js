9bcb54afeebfbc4e7e79fc471813c689f9572176
Sara Pulido <sara01rizo@gmail.com>
2021-11-08 13:38:46 +0100

    Added footer element with links to linked in and github project

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">My React Weather App</div>;
+  return (
+    <div className="App">
+      My React Weather App
+      <footer>
+        <a
+          href="https://github.com/sara01rizo/my-react-weather-app"
+          target="_blank"
+        >
+          Open source coded by{" "}
+          <a href="https://www.linkedin.com/in/sara-andrea-pulido-buenrostro-750b181b3/">
+            Sara Pulido
+          </a>
+          ✌
+        </a>
+      </footer>
+    </div>
+  );
 }
