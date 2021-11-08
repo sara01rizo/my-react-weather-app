df2b0623e733dc6d1ea1965bf0c1eb08dc4245dc
Sara Pulido <sara01rizo@gmail.com>
2021-11-08 13:52:55 +0100

    Added rel="noreferrer" to links inside the footer

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      My React Weather App
+      <h1>My React Weather App</h1>
       <footer>
+        <span>This project is an </span>
         <a
           href="https://github.com/sara01rizo/my-react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
-          Open source coded by{" "}
-          <a href="https://www.linkedin.com/in/sara-andrea-pulido-buenrostro-750b181b3/">
-            Sara Pulido
-          </a>
-          ✌
+          Open source
         </a>
+        <span> coded by </span>
+        <a
+          href="https://www.linkedin.com/in/sara-andrea-pulido-buenrostro-750b181b3/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Sara Pulido
+        </a>
+        ✌
       </footer>
     </div>
   );
