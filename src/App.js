9229a42a403f9dee7ce194fcdcc42286c5e12a09
Sara Pulido <sara01rizo@gmail.com>
2021-11-12 16:36:54 +0100

    Added link to Netlify - update Weather css

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>My React Weather App</h1>
-        <Weather defaultCity="Berlín" />
+        <Weather defaultCity="BERLIN" />
         <footer>
           <span>This project is an </span>
           <a
             href="https://github.com/sara01rizo/my-react-weather-app"
             target="_blank"
             rel="noreferrer"
+            alt="Github"
           >
             Open source
           </a>
@@ -22,10 +22,23 @@ export default function App() {
             href="https://www.linkedin.com/in/sara-andrea-pulido-buenrostro-750b181b3/"
             target="_blank"
             rel="noreferrer"
+            alt="Linkedin"
           >
             Sara Pulido
           </a>
           ✌
+          <span>
+            {" "}
+            and hosted on{" "}
+            <a
+              href="https://suspicious-yalow-89ec88.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              alt="Netlify"
+            >
+              Netlify
+            </a>{" "}
+          </span>
         </footer>
       </div>
     </div>
