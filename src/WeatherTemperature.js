ef8236393c44c7fcb6b9c3f9e475b433873cc6aa
Sara Pulido <sara01rizo@gmail.com>
2021-11-12 17:00:29 +0100

    Update function convertFahrenheit - onClick

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,11 +2,20 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
+  function convertFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
   if (unit === "celsius") {
     return (
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">°C</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={convertFahrenheit}>
+            °F
+          </a>{" "}
+        </span>
       </div>
     );
   } else {
