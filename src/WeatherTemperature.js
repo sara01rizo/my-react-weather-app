0fcc4bd4c6394a8ab7501be63ee7a03fcc45a976
Sara Pulido <sara01rizo@gmail.com>
2021-11-12 16:53:17 +0100

    Statement initial - WeatherTemperature js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="WeatherTemperature">
-      <span className="temperature">{Math.round(props.celsius)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">°C</span>
+      </div>
+    );
+  } else {
+    return "Fahrenheit";
+  }
 }
